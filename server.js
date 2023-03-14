dd91f2f398e3546e9ec4a621e065a97d146f73fe
Fadi Haddad <fadi.k.haddad@gmail.com>
2023-03-14 07:54:50 +0300

    Solve task 2

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const app = express();
 
+
 app.get("/", (req, res) => {
     res.send("Hello World");
+        const {x, y} = req.query;
+        const power = x**y;
+        res.send(200, power);
 });
 
 app.listen(8000, (req, res) => {
